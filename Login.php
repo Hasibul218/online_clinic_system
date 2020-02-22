8781e3b5c89769b4562ed962b755c659fd1b2872
hashibul218 <hashibul218@outlook.com>
2020-02-22 21:40:51 +0600

    Login Page Add

---
+++ Login.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<title>Login</title>
+	</head>
+	<body>
+		<form method="" action="">
+			<table align="center">
+				<tr>
+					<td colspan="2" align="center">
+						<h3 style="color:red">Online Clinic<h3>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="text" name="uname" value="" placeholder="User Name" required>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="text" name="pass" value="" placeholder="Password" required>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2" align="center">
+						<input type="submit" name="login" value="Login"><br>
+						<span><b>Or Register <a href="">here</a></b></span>
+					</td>
+				</tr>
+			</table>
+		</form>	
+	</body>
+</html>
