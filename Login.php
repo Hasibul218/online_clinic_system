a2625842309ae094645e433997e811904971e97f
hashibul218 <hashibul218@outlook.com>
2020-03-04 20:35:48 +0600

    Modify Login Page

--- Login.php
+++ Login.php
@@ -1,32 +1,42 @@
 <html>
 	<head>
 		<title>Login</title>
+				<link rel="stylesheet"type="text/css"href="CSS/logincss.css">
 	</head>
 	<body>
+		<div class="btngrp">
+			<button type="button"name="home"class="button">Home</button>
+			<button type="button"name="home"class="button">Login</button>
+		</div>
 		<form method="" action="">
-			<table align="center">
-				<tr>
-					<td colspan="2" align="center">
-						<h3 style="color:red">Online Clinic<h3>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<input type="text" name="uname" value="" placeholder="User Name" required>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<input type="text" name="pass" value="" placeholder="Password" required>
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2" align="center">
-						<input type="submit" name="login" value="Login"><br>
-						<span><b>Or Register <a href="HomePage.php">here</a></b></span>
-					</td>
-				</tr>
-			</table>
+			<div class="login">
+				<table>
+					<tr>
+						<td>
+							<input type="text" class="field" name="uname" value="" placeholder="User Name">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<br>
+							<input type="text" class="field" name="pass" value="" placeholder="Password">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<!--login button here -->
+							<br>
+							<button name="submit"class="submit">Login</button><br><br>
+							<label class="lbl"><b>Or Register <a href="HomePage.php">here</a></b></label>
+						</td>
+					</tr>
+				</table>
+			</div>
 		</form>	
+		<div class="div1">
+			<a href=""class="d1">About us |</a>
+			<a href=""class="d1">FAQ| </a>
+			<a href=""class="d1"> Privacy Policy</a>
+		</div>
 	</body>
 </html>
