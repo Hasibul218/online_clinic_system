515fdff4cc44533a01be4c016d0b75b9f9347112
hashibul218 <hashibul218@outlook.com>
2020-03-06 11:20:11 +0600

    Clinic List Add

---
+++ AdminClinicList.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>
+			Admin Clinic List
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/admincliniclist.css">
+	</head>
+	<body>
+		<div class="div1">
+			<h2>Clinic List</h2>
+		</div>
+	</body>
+</html>
