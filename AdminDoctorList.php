3fb3f6e4165f1444834388b98d7cf791cdba9ee4
hashibul218 <hashibul218@outlook.com>
2020-03-06 11:36:36 +0600

    Add Doctor List

---
+++ AdminDoctorList.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>
+			Admin Doctor List
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/admincliniclist.css">
+	</head>
+	<body>
+		<div class="div1">
+			<h2>Doctor List</h2>
+		</div>
+	</body>
+</html>
