9b75527fa38687478e6b362d59d1d83273e11e5d
hashibul218 <hashibul218@outlook.com>
2020-03-06 11:38:06 +0600

    Add Patient History

---
+++ AdminPatientHistory.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>
+			Admin Patient History
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/admincliniclist.css">
+	</head>
+	<body>
+		<div class="div1">
+			<h2>Patient Records</h2>
+		</div>
+	</body>
+</html>
