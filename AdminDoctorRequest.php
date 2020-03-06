721dfbc383ae246cabe50f7ee6c4497a7a9a8b58
hashibul218 <hashibul218@outlook.com>
2020-03-06 11:37:07 +0600

    Add Doctor Request

---
+++ AdminDoctorRequest.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>
+			Admin Doctor Request
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/admincliniclist.css">
+	</head>
+	<body>
+		<div class="div1">
+			<h2>Doctor Request List</h2>
+		</div>
+	</body>
+</html>
