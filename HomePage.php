761aaa99311a217bb5b448cfd90baf8fa5f4f5b0
hashibul218 <hashibul218@outlook.com>
2020-03-06 09:25:23 +0600

    Add Home Page

--- HomePage.php
+++ HomePage.php
@@ -1,12 +1,27 @@
 <html>
-	<head><title>Home Page</title></head>
+	<head><title>Home Page</title>
+			<link rel="stylesheet"type="text/css"href="CSS/homepage.css">
+
+	</head>
 	<body>
-		<span><h1 align ="center">Welcome to Online Clinic</h1></span>
-		<span><a href="Login.php"><h3 align="center"style="color:red">Sign in</h3></a></span>
-		<h2 align="center">Sign up as</h2>
-		<h4 align="center"style="color:green">&darr;</h4>   <!--For down arrow-->
-		<h4 align="center"style="color:green">&darr;</h4>
-		<span><a href="DoctorRegistration.php"><h3 align="center"style="color:red">Doctor</h3></a></span>
-		<span><a href="PatientRegistration.php"><h3 align="center"style="color:red">Patient</h3></a></span>
+		<div class="btngrp">
+			<label class="h1">Welcome to Online Clinic</label>
+			<button type="button"name="home"class="button">Home
+			</button>
+			<button type="button"name="home"class="button">Login</button>
+			
+			<div class="dropdown">
+				<button class="dropbtn">Sign up 
+				</button>
+				<div class="dropdown-content">
+				  <a href="DoctorRegistration.php">Doctor</a>
+				  <a href="PatientRegistration.php">Patient</a>
+				</div>
+			</div> 
+		</div>
+		<h2>Clinic For Doctor & Patient</h2>
+		<div class="anime">
+			
+		</div>
 	</body>
 </html>
