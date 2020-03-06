f87d51954f91de71736059791a01b8ffaa6bd191
hashibul218 <hashibul218@outlook.com>
2020-03-06 15:11:19 +0600

    Doctor Home Page Modify

---
+++ DoctorHomePage.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>
+			Doctor Home Page
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/doctorhomepage.css">
+	</head>
+	<body>
+		<div class="div1">
+			<h2>Home Page</h2>
+		</div>
+		<label class="l1">Welcome Doctor<label>
+		<div class="sidebar">                <!--https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav_dropdown-->
+			<a href="#about">Profile</a>
+			<a href="#services">Set Schedule</a>
+			<a href="#clients">Patient Wating</a>
+			<a href="#contact">Patient Records</a>
+		</div>
+	</body>
+</html>
