0059f215444907bdfda7e10b43a45efbe03ee41c
hashibul218 <hashibul218@outlook.com>
2020-03-06 11:38:37 +0600

    Add Patient List

---
+++ AdminPatientList.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>
+			Admin Patient List
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/admincliniclist.css">
+	</head>
+	<body>
+		<div class="div1">
+			<h2>Patient List</h2>
+		</div>
+	</body>
+</html>
