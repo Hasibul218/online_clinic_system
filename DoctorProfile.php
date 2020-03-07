e70c2f42369bfe63a113e2b90eaf87e478efb3df
hashibul218 <hashibul218@outlook.com>
2020-03-07 10:03:43 +0600

    Doctor Profile

---
+++ DoctorProfile.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>
+			Doctor Profile
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/doctorcss.css">
+	</head>
+	<button type="button"name="home"class="button"onclick="window.location='DoctorHomePage.php'">Home Page</button>
+	<body>
+		<div class="div1">
+			<h2>My Profile</h2>
+		</div>
+	</body>
+</html>
