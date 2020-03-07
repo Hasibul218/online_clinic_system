46fff7d37cbeb573d8109024044598e7f66a68fa
hashibul218 <hashibul218@outlook.com>
2020-03-07 10:08:03 +0600

    Patient Notification

---
+++ PatientNotification.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>
+			Notification
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/doctorcss.css">
+	</head>
+	<button type="button"name="home"class="button"onclick="window.location='PatientHomePage.php'">Home Page</button>
+	<body>
+		<div class="div1">
+			<h2>Notification</h2>
+		</div>
+	</body>
+</html>
