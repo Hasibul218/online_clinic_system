af6450d7faa12c31e05405b07cb298ce1a923e7d
hashibul218 <hashibul218@outlook.com>
2020-03-07 10:09:08 +0600

    Patient Prescription

---
+++ PatientPrescription.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>
+			Prescription
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/doctorcss.css">
+	</head>
+	<button type="button"name="home"class="button"onclick="window.location='PatientHomePage.php'">Home Page</button>
+	<body>
+		<div class="div1">
+			<h2>Prescription</h2>
+		</div>
+	</body>
+</html>
