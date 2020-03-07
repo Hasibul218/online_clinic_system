4fc21987700e25b08290d74355bf45a6a699208f
hashibul218 <hashibul218@outlook.com>
2020-03-07 10:10:27 +0600

    Take Appointment

---
+++ PatientTakeAppointment.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>
+			Take Appointment
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/doctorcss.css">
+	</head>
+	<button type="button"name="home"class="button"onclick="window.location='PatientHomePage.php'">Home Page</button>
+	<body>
+		<div class="div1">
+			<h2>Take Appointment</h2>
+		</div>
+	</body>
+</html>
