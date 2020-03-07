e0e4bd3aef9e95c4643dddd4798c2925ea6909d3
hashibul218 <hashibul218@outlook.com>
2020-03-07 10:07:00 +0600

    Patient Home

---
+++ PatientHomePage.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>
+			Patient Home Page
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/doctorcss.css">
+		<link rel="stylesheet"type="text/css"href="CSS/doctorhomepage.css">
+	</head>
+	<body>
+		<button class="button"onclick="window.location='Login.php'">Logout</button>
+		<div class="div1">
+			<h2>Home Page</h2>
+		</div>
+		<label class="l1">Welcome Patient<label>
+		<div class="sidebar">                <!--https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav_dropdown-->
+			<a href="PatientProfile.php">Profile</a>
+			<a href="PatientTakeAppointment.php">Take Appoinment</a>
+			<a href="PatientNotification.php">Prescription</a>
+			<a href="PatientNotification.php">Noification</a>
+		</div>
+	</body>
+</html>
