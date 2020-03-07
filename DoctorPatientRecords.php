6ed6c001df1068950584d1afde6681b2454a833f
hashibul218 <hashibul218@outlook.com>
2020-03-07 09:14:55 +0600

    Patient Records

---
+++ DoctorPatientRecords.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>
+			Doctor Patient Records
+		</title>
+		<link rel="stylesheet"type="text/css"href="CSS/doctorcss.css">
+	</head>
+	<body>
+		<button class="button"onclick="window.location='DoctorHomePage.php'">Home</button>
+		<div class="div1">
+
+			<h2>Patient Records</h2>
+
+		</div>
+
+	</body>
+</html>
