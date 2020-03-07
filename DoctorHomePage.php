afddac2963a6bb8acaff6d9e3f670141a44641c0
hashibul218 <hashibul218@outlook.com>
2020-03-07 10:14:50 +0600

    Modify Doctor Home Page

--- DoctorHomePage.php
+++ DoctorHomePage.php
@@ -4,17 +4,19 @@
 			Doctor Home Page
 		</title>
 		<link rel="stylesheet"type="text/css"href="CSS/doctorhomepage.css">
+		<link rel="stylesheet"type="text/css"href="CSS/doctorcss.css">
 	</head>
 	<body>
+		<button class="button"onclick="window.location='Login.php'">Logout</button>
 		<div class="div1">
 			<h2>Home Page</h2>
 		</div>
 		<label class="l1">Welcome Doctor<label>
 		<div class="sidebar">                <!--https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav_dropdown-->
-			<a href="#about">Profile</a>
-			<a href="#services">Set Schedule</a>
-			<a href="#clients">Patient Wating</a>
-			<a href="#contact">Patient Records</a>
+			<a href="DoctorProfile.php">Profile</a>
+			<a href="DoctorSetSchedule.php">Set Schedule</a>
+			<a href="DoctorPatientWaiting.php">Patient Wating</a>
+			<a href="DoctorPatientRecords.php">Patient Records</a>
 		</div>
 	</body>
 </html>
