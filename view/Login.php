0acb1d8fce0741efe41a381a0ca75def66f4f04f
hashibul218 <hashibul218@outlook.com>
2020-03-25 02:59:38 +0600

    modify

--- view/Login.php
+++ view/Login.php
@@ -2,11 +2,9 @@
 	<head>
 		<title>Login</title>
 		<link rel="stylesheet"type="text/css"href="CSS/logincss.css">
-	</head>
-	<body>
-	
+		<!--show password checkbox starts-->
 		<script>
-		function myFunction() {
+		function showpass() {
 		  var x = document.getElementById("myinput");
 		  if (x.type === "password") {
 			x.type = "text";
@@ -15,32 +13,45 @@
 		  }
 		}
 		</script>
+		<!--show password checkbox ends-->
+	</head>
+	<body>
+	
+		
 		
 		<div class="btngrp">
 			<label class="ll">Online Clinic</label>
 			<button type="button"name="home"class="button"onclick="window.location='HomePage.php'">Home</button>
 			<button type="button"name="home"class="button"onclick="window.location='Login.php'">Login</button>
 		</div>
-		<form method="" action="">
+		<form method="post" action="../control/LoginControl.php">
 			<div class="login">
+				<div class="err_mgs">
+					<?php
+						if(@$_GET['empty']==true)
+						{
+							echo $_GET['empty'];
+						}
+					?>
+				</div>
 				<table>
 					<tr>
 						<td>
-							<input type="text" class="field" name="uname" value="" placeholder="User Name">
+							<input type="text" class="field" name="uid"placeholder="User Id">
 						</td>
 					</tr>
 					<tr>
 						<td>
 							<br>
-							<input type="password" class="field" name="pass" value="" placeholder="Password"id="myinput"><br><br>
-							<input type="checkbox" onclick="myFunction()"> &nbsp Show Password
+							<input type="password" class="field" name="pass" placeholder="Password"id="myinput"><br><br>
+							<input type="checkbox" onclick="showpass()"> &nbsp Show Password
 						</td>
 					</tr>
 					<tr>
 						<td>
 							<!--login button here -->
 							<br>
-							<button name="submit"class="submit">Login</button><br><br>
+							<button name="login"class="submit">Login</button><br><br>
 							<label class="lbl"><b>Or Register <a href="HomePage.php">here</a></b></label>
 						</td>
 					</tr>
