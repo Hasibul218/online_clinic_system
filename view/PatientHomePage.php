8efe97b1e9c41ba9fda14b7e74e0567d11ef8bae
hashibul218 <hashibul218@outlook.com>
2020-03-25 15:57:44 +0600

    modify

--- view/PatientHomePage.php
+++ view/PatientHomePage.php
@@ -1,3 +1,10 @@
+<?php
+	session_start();
+	if(!isset($_SESSION['uid']))
+	{
+		header("Location:Login.php");
+	}
+?>
 <html>
 	<head>
 		<title>
@@ -7,12 +14,12 @@
 		<link rel="stylesheet"type="text/css"href="CSS/doctorhomepage.css">
 	</head>
 	<body>
-		<button class="button"onclick="window.location='Login.php'">Logout</button>
+		<button class="button"onclick="window.location='../control/LogoutControl.php'">Logout</button>
 		<button class="button"onclick="window.location='PatientNotification.php'">Noification</button>
 		<div class="div1">
 			<h2>Home Page</h2>
 		</div>
-		<label class="l1">Welcome Patient<label>
+		<label class="l1">Welcome Patient :<?php echo $_SESSION['uid'] ?><label>
 		<div class="sidebar">                <!--https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav_dropdown-->
 			<a href="PatientProfile.php">Profile</a>
 			<a href="PatientTakeAppointment.php">Take Appoinment</a>
