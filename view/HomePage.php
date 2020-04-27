e39dd57cca89eb7eee9cde4b5372cc49db60ccb1
hashibul218 <hashibul218@outlook.com>
2020-04-27 11:16:48 +0600

    footer part add

--- view/HomePage.php
+++ view/HomePage.php
@@ -1,6 +1,7 @@
 <html>
 	<head><title>Home Page</title>
 			<link rel="stylesheet"type="text/css"href="CSS/homepage.css">
+			<link rel="stylesheet" href="CSS/faq.css">
 
 	</head>
 	<body>
@@ -19,16 +20,19 @@
 				</div>
 			</div> 
 		</div>
-		<h2>Clinic For Doctor & Patient</h2>
-		<div class="maindiv"></div>
-		<div class="maindiv2"></div>
-		<div class="maindiv3">
-			<p class="p">
-				Call us at +8801747979703
-				Website: www.onlineclinic.com
-				<a href="https://www.facebook.com/onlineclinic.helth/">visit our facebook page</a>
-				<a href="https://www.mhealth.org/Care/Conditions/COVID-19"> <br>->CORONAVIRUS (COVID-19)</a> The information you need to know
-			</p>
+		<div class="home">
+			<h2>Clinic For Doctor & Patient</h2>
+			<div class="maindiv"></div>
+			<div class="maindiv2"></div>
+			<div class="maindiv3">
+				<p class="p">
+					Call us at +8801747979703
+					Website: www.onlineclinic.com
+					<a href="https://www.facebook.com/onlineclinic.helth/">visit our facebook page</a>
+					<a href="https://www.mhealth.org/Care/Conditions/COVID-19"> <br>->CORONAVIRUS (COVID-19)</a> The information you need to know
+				</p>
+			</div>
 		</div>
+		<?php include 'FAQ.php'; ?>
 	</body>
 </html>
